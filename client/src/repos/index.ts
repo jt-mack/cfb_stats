58288e5237725bc8d63c01a6f5feabce18fee950
jmccormick@tarkenton.com <jmccormick@tarkenton.com>
2026-09-15 15:17:49 -0400

    Remove client directory and all associated files
    - Deleted the entire client directory, including all components, assets, and configuration files.
    - This removal simplifies the project structure and prepares for a new implementation.

--- client/src/repos/index.ts
+++
@@ -1,6 +0,0 @@
-export { get, ApiError } from './apiClient';
-export * from './types';
-export * from './fbsRepo';
-export * from './conferencesRepo';
-export * from './teamsRepo';
-export * from './gamesRepo';
